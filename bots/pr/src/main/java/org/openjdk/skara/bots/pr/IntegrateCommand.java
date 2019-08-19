85af475cfd24fd5f16368f3329085b2eef3f9e0a
Robin Westberg <robin.westberg@oracle.com>
2019-08-19 14:24:47 +0200

    Add integrated label to PRs when integrating them

--- bots/pr/src/main/java/org/openjdk/skara/bots/pr/IntegrateCommand.java
+++ bots/pr/src/main/java/org/openjdk/skara/bots/pr/IntegrateCommand.java
@@ -97,6 +97,7 @@ public class IntegrateCommand implements CommandHandler {
                 reply.println("Pushed as commit " + rebasedHash.get().hex() + ".");
                 prInstance.localRepo().push(rebasedHash.get(), pr.repository().getUrl(), pr.getTargetRef());
                 pr.setState(PullRequest.State.CLOSED);
+                pr.addLabel("integrated");
             }
         } catch (IOException e) {
             log.throwing("IntegrateCommand", "handle", e);
