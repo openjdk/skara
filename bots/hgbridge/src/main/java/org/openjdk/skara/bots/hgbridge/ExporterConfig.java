a8f5474a340144fb8a1dc5466efe285c5be970d4
Robin Westberg <robin.westberg@oracle.com>
2019-11-27 10:42:28 +0100

    All materialize-to-local-ref should force update

--- bots/hgbridge/src/main/java/org/openjdk/skara/bots/hgbridge/ExporterConfig.java
+++ bots/hgbridge/src/main/java/org/openjdk/skara/bots/hgbridge/ExporterConfig.java
@@ -169,7 +169,7 @@ class ExporterConfig {
 
     public Converter resolve(Path scratchPath) throws IOException {
         var localRepo = Repository.materialize(scratchPath, configurationRepo.url(),
-                                               configurationRef + ":hgbridge_config_" + configurationRepo.name());
+                                               "+" + configurationRef + ":hgbridge_config_" + configurationRepo.name());
 
         var replacements = parseMap(localRepo.root(), replacementsFile,
                                     field -> new Hash(field.name()),
