89cba77e084882537c5f145739eb9d84035e7bd4
Erik Helin <ehelin@openjdk.org>
2020-07-07 10:27:33 +0000

    pr: fix merge tests
    Reviewed-by: jvernee

--- bots/pr/src/test/java/org/openjdk/skara/bots/pr/MergeTests.java
+++ bots/pr/src/test/java/org/openjdk/skara/bots/pr/MergeTests.java
@@ -1064,7 +1064,7 @@ class MergeTests {
             TestBotRunner.runPeriodicItems(mergeBot);
 
             // There should be a warning
-            assertLastCommentContains(pr, "This pull request contains merges that brings in commits not present");
+            assertLastCommentContains(pr, "This pull request contains merges that bring in commits not present");
         }
     }
 }
