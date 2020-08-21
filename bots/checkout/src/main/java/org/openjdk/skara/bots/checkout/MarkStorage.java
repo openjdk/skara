2cac8ee8db4017d9fab15d6e7f8a7c7ca1af74d7
Erik Helin <erik.helin@oracle.com>
2020-08-21 13:51:25 +0200

    bots: add checkout bot

---
+++ bots/checkout/src/main/java/org/openjdk/skara/bots/checkout/MarkStorage.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2020, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+package org.openjdk.skara.bots.checkout;
+
+import org.openjdk.skara.forge.HostedRepository;
+import org.openjdk.skara.vcs.Author;
+import org.openjdk.skara.vcs.Hash;
+import org.openjdk.skara.vcs.openjdk.convert.Mark;
+import org.openjdk.skara.storage.StorageBuilder;
+
+import java.net.URI;
+import java.util.*;
+import java.util.stream.Collectors;
+
+class MarkStorage {
+    private static Mark deserializeMark(String s) {
+        var parts = s.split(" ");
+        if (!(parts.length == 3 || parts.length == 4)) {
+            throw new IllegalArgumentException("Unexpected string:" + s);
+        }
+
+        var key = Integer.parseInt(parts[0]);
+        var hg = new Hash(parts[1]);
+        var git = new Hash(parts[2]);
+
+        return parts.length == 3 ? new Mark(key, hg, git) : new Mark(key, hg, git, new Hash(parts[3]));
+    }
+
+    private static String serialize(Collection<Mark> added, Set<Mark> existing) {
+        var marks = new ArrayList<Mark>();
+        var handled = new HashSet<Integer>();
+        for (var mark : added) {
+            marks.add(mark);
+            handled.add(mark.key());
+        }
+        for (var mark : existing) {
+            if (!handled.contains(mark.key())) {
+                marks.add(mark);
+            }
+        }
+        Collections.sort(marks);
+        var sb = new StringBuilder();
+        for (var mark : marks) {
+            sb.append(Integer.toString(mark.key()));
+            sb.append(" ");
+            sb.append(mark.hg().hex());
+            sb.append(" ");
+            sb.append(mark.git().hex());
+            if (mark.tag().isPresent()) {
+                sb.append(" ");
+                sb.append(mark.tag().get().hex());
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
+    private static Set<Mark> deserialize(String current) {
+        var res = current.lines()
+                         .map(MarkStorage::deserializeMark)
+                         .collect(Collectors.toSet());
+        return res;
+    }
+
+    static StorageBuilder<Mark> create(HostedRepository repo, Author user, String name) {
+        return new StorageBuilder<Mark>(name + ".marks.txt")
+            .remoteRepository(repo, "master", user.name(), user.email(), "Updated marks for " + name)
+            .serializer(MarkStorage::serialize)
+            .deserializer(MarkStorage::deserialize);
+    }
+}
---
+++ bots/checkout/src/test/java/org/openjdk/skara/bots/checkout/CheckoutBotTests.java
@@ -0,0 +1,130 @@
+/*
+ * Copyright (c) 2020, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package org.openjdk.skara.bots.checkout;
+
+import org.openjdk.skara.forge.HostedRepository;
+import org.openjdk.skara.storage.StorageBuilder;
+import org.openjdk.skara.test.*;
+import org.openjdk.skara.host.HostUser;
+import org.openjdk.skara.vcs.Tag;
+import org.openjdk.skara.vcs.*;
+
+import org.junit.jupiter.api.*;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.*;
+import static java.nio.file.StandardOpenOption.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CheckoutBotTests {
+    private static void populate(Repository r) throws IOException {
+        var readme = r.root().resolve("README");
+        Files.write(readme, List.of("Hello, readme!"));
+
+        r.add(readme);
+        r.commit("Add README", "duke", "duke@openjdk.java.net");
+
+        Files.write(readme, List.of("Another line"), WRITE, APPEND);
+        r.add(readme);
+        r.commit("Modify README", "duke", "duke@openjdk.java.net");
+
+        Files.write(readme, List.of("A final line"), WRITE, APPEND);
+        r.add(readme);
+        r.commit("Final README", "duke", "duke@openjdk.java.net");
+    }
+
+    @Test
+    void simpleConversion(TestInfo testInfo) throws IOException {
+        try (var tmp = new TemporaryDirectory()) {
+            var host = TestHost.createNew(List.of(new HostUser(0, "duke", "J. Duke")));
+            var marksLocalDir = tmp.path().resolve("marks.git");
+            Files.createDirectories(marksLocalDir);
+            var marksLocalRepo = Repository.init(marksLocalDir, VCS.GIT);
+            marksLocalRepo.config("receive", "denyCurrentBranch", "ignore");
+            var marksHostedRepo = new TestHostedRepository(host, "marks", marksLocalRepo);
+
+            var storage = tmp.path().resolve("storage");
+            var scratch = tmp.path().resolve("scratch");
+            var marksAuthor = new Author("duke", "duke@openjdk.org");
+            var marksStorage = MarkStorage.create(marksHostedRepo, marksAuthor, "test");
+
+            var hgDir = tmp.path().resolve("hg");
+
+            var gitLocalDir = tmp.path().resolve("from.git");
+            Files.createDirectories(gitLocalDir);
+            var gitLocalRepo = Repository.init(gitLocalDir, VCS.GIT);
+            populate(gitLocalRepo);
+            var gitHostedRepo = new TestHostedRepository(host, "from", gitLocalRepo);
+
+            var bot = new CheckoutBot(gitHostedRepo.url(), gitLocalRepo.defaultBranch(), hgDir, storage, marksStorage);
+            var runner = new TestBotRunner();
+            runner.runPeriodicItems(bot);
+
+            var hgRepo = Repository.get(hgDir).orElseThrow();
+            assertEquals(3, hgRepo.commitMetadata().size());
+        }
+    }
+
+    @Test
+    void update(TestInfo testInfo) throws IOException {
+        try (var tmp = new TemporaryDirectory()) {
+            var host = TestHost.createNew(List.of(new HostUser(0, "duke", "J. Duke")));
+            var marksLocalDir = tmp.path().resolve("marks.git");
+            Files.createDirectories(marksLocalDir);
+            var marksLocalRepo = Repository.init(marksLocalDir, VCS.GIT);
+            marksLocalRepo.config("receive", "denyCurrentBranch", "ignore");
+            var marksHostedRepo = new TestHostedRepository(host, "marks", marksLocalRepo);
+
+            var storage = tmp.path().resolve("storage");
+            var scratch = tmp.path().resolve("scratch");
+            var marksAuthor = new Author("duke", "duke@openjdk.org");
+            var marksStorage = MarkStorage.create(marksHostedRepo, marksAuthor, "test");
+            var runner = new TestBotRunner();
+
+            var hgDir = tmp.path().resolve("hg");
+
+            var gitLocalDir = tmp.path().resolve("from.git");
+            Files.createDirectories(gitLocalDir);
+            var gitLocalRepo = Repository.init(gitLocalDir, VCS.GIT);
+            populate(gitLocalRepo);
+            var gitHostedRepo = new TestHostedRepository(host, "from", gitLocalRepo);
+
+            var bot = new CheckoutBot(gitHostedRepo.url(), gitLocalRepo.defaultBranch(), hgDir, storage, marksStorage);
+            runner.runPeriodicItems(bot);
+
+            var hgRepo = Repository.get(hgDir).orElseThrow();
+            assertEquals(3, hgRepo.commitMetadata().size());
+            assertEquals(3, gitLocalRepo.commitMetadata().size());
+
+            var readme = gitLocalRepo.root().resolve("README");
+            Files.write(readme, List.of("An updated line"), WRITE, APPEND);
+            gitLocalRepo.add(readme);
+            gitLocalRepo.commit("Updated Final README", "duke", "duke@openjdk.java.net");
+
+            runner.runPeriodicItems(bot);
+            assertEquals(4, hgRepo.commitMetadata().size());
+        }
+    }
+}
