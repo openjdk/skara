bb7fb3ca72b8119f66f2d344abc5b6acc2279dd9
Erik Helin <erik.helin@oracle.com>
2020-09-28 11:41:49 +0200

    vcs: allow capital letters in e-mails

--- vcs/src/main/java/org/openjdk/skara/vcs/openjdk/CommitMessageSyntax.java
+++ vcs/src/main/java/org/openjdk/skara/vcs/openjdk/CommitMessageSyntax.java
@@ -26,7 +26,7 @@ import java.util.regex.Pattern;
 
 public class CommitMessageSyntax {
         private static final String OPENJDK_USERNAME_REGEX = "[-.a-z0-9]+";
-        public static final String EMAIL_ADDR_REGEX = "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]+";
+        public static final String EMAIL_ADDR_REGEX = "[A-Za-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]+";
         public static final String REAL_NAME_REGEX = "[^<>@]+";
         private static final String REAL_NAME_AND_EMAIL_ATTR_REGEX = REAL_NAME_REGEX + " +<" + EMAIL_ADDR_REGEX + ">";
         private static final String ATTR_REGEX = "(?:(?:" + EMAIL_ADDR_REGEX + ")|(?:" + REAL_NAME_AND_EMAIL_ATTR_REGEX + "))";
