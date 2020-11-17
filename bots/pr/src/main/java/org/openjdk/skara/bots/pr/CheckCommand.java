edcd068d1799044e4fac2f77d150db74846cd715
Kartik Ohri <kartikohri13@gmail.com>
2020-11-17 12:13:39 +0530

    Do not alter bot recheck schedule

--- bots/pr/src/main/java/org/openjdk/skara/bots/pr/CheckCommand.java
+++ bots/pr/src/main/java/org/openjdk/skara/bots/pr/CheckCommand.java
@@ -1,11 +1,15 @@
 package org.openjdk.skara.bots.pr;
 
+import org.openjdk.skara.forge.HostedRepositoryPool;
 import org.openjdk.skara.forge.PullRequest;
+import org.openjdk.skara.forge.PullRequestUtils;
 import org.openjdk.skara.issuetracker.Comment;
 
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.UncheckedIOException;
 import java.nio.file.Path;
-import java.time.Instant;
+import java.util.HashSet;
 import java.util.List;
 
 public class CheckCommand implements CommandHandler {
@@ -16,7 +20,22 @@ public class CheckCommand implements CommandHandler {
 
     @Override
     public void handle(PullRequestBot bot, PullRequest pr, CensusInstance censusInstance, Path scratchPath, CommandInvocation command, List<Comment> allComments, PrintWriter reply) {
-        bot.scheduleRecheckAt(pr, Instant.now());
+        try {
+            var workItem = new CheckWorkItem(bot, pr, e -> {});
+
+            var path = scratchPath.resolve("check").resolve(pr.repository().name());
+            var seedPath = bot.seedStorage().orElse(scratchPath.resolve("seeds"));
+            var hostedRepositoryPool = new HostedRepositoryPool(seedPath);
+            var localRepo = PullRequestUtils.materialize(hostedRepositoryPool, pr, path);
+
+            var allReviews = pr.reviews();
+            var activeReviews = CheckablePullRequest.filterActiveReviews(allReviews);
+            var activeLabels = new HashSet<>(pr.labels());
+
+            CheckRun.execute(workItem, pr, localRepo, allComments, allReviews, activeReviews, activeLabels, censusInstance, bot.ignoreStaleReviews());
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
     }
 
 }
