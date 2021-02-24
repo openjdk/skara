751856ddd56306f51ec6956ed732baf13f9962eb
Erik Helin <erik.helin@oracle.com>
2021-02-24 15:37:42 +0100

    pr: only act on commit comments on known branches

--- bots/pr/src/test/java/org/openjdk/skara/bots/pr/CommitCommandTests.java
+++ bots/pr/src/test/java/org/openjdk/skara/bots/pr/CommitCommandTests.java
@@ -25,9 +25,12 @@ package org.openjdk.skara.bots.pr;
 import org.junit.jupiter.api.*;
 import org.openjdk.skara.issuetracker.Issue;
 import org.openjdk.skara.test.*;
+import org.openjdk.skara.vcs.Hash;
 
 import java.io.IOException;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CommitCommandTests {
     @Test
@@ -119,6 +122,46 @@ public class CommitCommandTests {
             pr.addComment("/integrate");
             TestBotRunner.runPeriodicItems(bot);
             PullRequestAsserts.assertLastCommentContains(pr, "can only be used in open pull requests");
+        }
+    }
+
+    @Test
+    void commitNotItRepository(TestInfo testInfo) throws IOException {
+        try (var credentials = new HostCredentials(testInfo);
+             var tempFolder = new TemporaryDirectory()) {
+            var author = credentials.getHostedRepository();
+            var reviewer = credentials.getHostedRepository();
+
+            var censusBuilder = credentials.getCensusBuilder()
+                                           .addAuthor(author.forge().currentUser().id())
+                                           .addReviewer(reviewer.forge().currentUser().id());
+            var seedFolder = tempFolder.path().resolve("seed");
+            var bot = PullRequestBot.newBuilder()
+                                    .repo(author)
+                                    .censusRepo(censusBuilder.build())
+                                    .censusLink("https://census.com/{{contributor}}-profile")
+                                    .seedStorage(seedFolder)
+                                    .build();
+
+            // Populate the projects repository
+            var localRepo = CheckableRepository.init(tempFolder.path(), author.repositoryType());
+            var masterHash = localRepo.resolve("master").orElseThrow();
+            localRepo.push(masterHash, author.url(), "master", true);
+
+            // Make a change directly on master
+            var editHash = CheckableRepository.appendAndCommit(localRepo);
+            localRepo.push(editHash, author.url(), "master");
+
+            // Make a commit only present in pr branch
+            var prHash = CheckableRepository.appendAndCommit(localRepo);
+            localRepo.push(prHash, author.url(), "pr/1", true);
+
+            // Add a help command to commit in pr branch
+            var comment = author.addCommitComment(prHash, "/help");
+            TestBotRunner.runPeriodicItems(bot);
+
+            // Verify that the bot did *not* reply
+            assertEquals(List.of(comment), author.commitComments(prHash));
         }
     }
 }
