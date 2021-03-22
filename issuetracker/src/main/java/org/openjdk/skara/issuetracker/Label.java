b216ebb47a9895e655e8876e1a138c87455dde4a
Erik Helin <erik.helin@oracle.com>
2021-03-22 09:42:04 +0100

    wip

---
+++ issuetracker/src/main/java/org/openjdk/skara/issuetracker/Label.java
@@ -0,0 +1,13 @@
+public class Label {
+    public Label(String name, String description) {
+        this.name = name;
+        this.description
+    }
+    public String name() {
+        return name;
+    }
+
+    public String description() {
+        return description;
+    }
+}
