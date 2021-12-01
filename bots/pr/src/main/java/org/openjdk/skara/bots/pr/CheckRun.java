08548eb733e3f37b558fee5ba6b3bd5380a9044c
Guoxiong Li <gli@openjdk.org>
2021-12-01 17:31:43 +0000

    1254: Add a checkbox for CSR requirement
    Reviewed-by: ihse, erikj

--- bots/pr/src/main/java/org/openjdk/skara/bots/pr/CheckRun.java
+++ bots/pr/src/main/java/org/openjdk/skara/bots/pr/CheckRun.java
@@ -198,6 +198,30 @@ class CheckRun {
         return ret;
     }
 
+    // Additional bot-specific progresses that are not handled by JCheck
+    private Map<String, Boolean> botSpecificProgresses() {
+        var ret = new HashMap<String, Boolean>();
+        if (pr.labelNames().contains("csr")) {
+            // If the PR have csr label, the CSR request need to be approved.
+            ret.put("Change requires a CSR request to be approved", false);
+        } else {
+            var csrIssue = Issue.fromStringRelaxed(pr.title()).flatMap(this::getCsrIssue)
+                    .flatMap(value -> issueProject() != null ? issueProject().issue(value.shortId()) : Optional.empty());
+            if (csrIssue.isPresent()) {
+                var resolution = csrIssue.get().properties().get("resolution");
+                if (resolution != null && !resolution.isNull()
+                        && resolution.get("name") != null && !resolution.get("name").isNull()
+                        && csrIssue.get().state() == org.openjdk.skara.issuetracker.Issue.State.CLOSED
+                        && "Approved".equals(resolution.get("name").asString())) {
+                    // The PR doesn't have csr label and the csr request has been Approved.
+                    ret.put("Change requires a CSR request to be approved", true);
+                }
+            }
+            // At other states, no need to add the csr progress.
+        }
+        return ret;
+    }
+
     private void setExpiration(Duration expiresIn) {
         // Use the shortest expiration
         if (this.expiresIn == null || this.expiresIn.compareTo(expiresIn) > 0) {
@@ -206,8 +230,7 @@ class CheckRun {
     }
 
     private Map<String, String> blockingIntegrationLabels() {
-        return Map.of("rejected", "The change is currently blocked from integration by a rejection.",
-                      "csr", "The change requires a CSR request to be approved.");
+        return Map.of("rejected", "The change is currently blocked from integration by a rejection.");
     }
 
     private List<String> botSpecificIntegrationBlockers() {
@@ -374,8 +397,9 @@ class CheckRun {
         }
     }
 
-    private String getChecksList(PullRequestCheckIssueVisitor visitor, boolean isCleanBackport) {
+    private String getChecksList(PullRequestCheckIssueVisitor visitor, boolean isCleanBackport, Map<String, Boolean> additionalProgresses) {
         var checks = isCleanBackport ? visitor.getReadyForReviewChecks() : visitor.getChecks();
+        checks.putAll(additionalProgresses);
         return checks.entrySet().stream()
                 .map(entry -> "- [" + (entry.getValue() ? "x" : " ") + "] " + entry.getKey())
                 .collect(Collectors.joining("\n"));
@@ -457,12 +481,12 @@ class CheckRun {
     }
 
     private String getStatusMessage(List<Comment> comments, List<Review> reviews, PullRequestCheckIssueVisitor visitor,
-                                    List<String> additionalErrors, List<String> integrationBlockers,
-                                    boolean isCleanBackport) {
+                                    List<String> additionalErrors, Map<String, Boolean> additionalProgresses,
+                                    List<String> integrationBlockers, boolean isCleanBackport) {
         var progressBody = new StringBuilder();
         progressBody.append("---------\n");
         progressBody.append("### Progress\n");
-        progressBody.append(getChecksList(visitor, isCleanBackport));
+        progressBody.append(getChecksList(visitor, isCleanBackport, additionalProgresses));
 
         var allAdditionalErrors = Stream.concat(visitor.hiddenMessages().stream(), additionalErrors.stream())
                                         .sorted()
@@ -933,6 +957,7 @@ class CheckRun {
             }
 
             List<String> additionalErrors = List.of();
+            Map<String, Boolean> additionalProgresses = Map.of();
             Hash localHash;
             try {
                 // Do not pass eventual original commit even for backports since it will cause
@@ -954,6 +979,7 @@ class CheckRun {
                 var additionalConfiguration = AdditionalConfiguration.get(localRepo, localHash, pr.repository().forge().currentUser(), comments);
                 checkablePullRequest.executeChecks(localHash, censusInstance, visitor, additionalConfiguration);
                 additionalErrors = botSpecificChecks(isCleanBackport);
+                additionalProgresses = botSpecificProgresses();
             }
             updateCheckBuilder(checkBuilder, visitor, additionalErrors);
             updateReadyForReview(visitor, additionalErrors);
@@ -961,7 +987,8 @@ class CheckRun {
             var integrationBlockers = botSpecificIntegrationBlockers();
 
             // Calculate and update the status message if needed
-            var statusMessage = getStatusMessage(comments, activeReviews, visitor, additionalErrors, integrationBlockers, isCleanBackport);
+            var statusMessage = getStatusMessage(comments, activeReviews, visitor, additionalErrors,
+                                                additionalProgresses, integrationBlockers, isCleanBackport);
             var updatedBody = updateStatusMessage(statusMessage);
             var title = pr.title();
 
@@ -975,11 +1002,13 @@ class CheckRun {
             var commitMessage = String.join("\n", commit.message());
             var readyForIntegration = visitor.messages().isEmpty() &&
                                       additionalErrors.isEmpty() &&
+                                      !additionalProgresses.containsValue(false) &&
                                       integrationBlockers.isEmpty();
             if (isCleanBackport) {
                 // Reviews are not needed for clean backports
                 readyForIntegration = visitor.isReadyForReview() &&
                                       additionalErrors.isEmpty() &&
+                                      !additionalProgresses.containsValue(false) &&
                                       integrationBlockers.isEmpty();
             }
 
