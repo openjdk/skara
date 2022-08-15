4dbccc451ed1dcffa685e923cfcb489c54acf537
Erik Joelsson <erikj@openjdk.org>
2022-08-15 10:18:04 -0700

    Added missing copyrights and expanded comment

--- bots/csr/src/main/java/org/openjdk/skara/bots/csr/CSRIssueBot.java
+++ bots/csr/src/main/java/org/openjdk/skara/bots/csr/CSRIssueBot.java
@@ -1,3 +1,25 @@
+/*
+ * Copyright (c) 2022, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
 package org.openjdk.skara.bots.csr;
 
 import java.time.Instant;
@@ -45,7 +67,9 @@ public class CSRIssueBot implements Bot {
     public List<WorkItem> getPeriodicItems() {
         var ret = new ArrayList<WorkItem>();
         // In the very first round, we just find the last updated issue to
-        // initialize lastUpdatedAt.
+        // initialize lastUpdatedAt. There is no need for reacting to any CSR
+        // issue update before that, as the CSRPullRequestBot will go through
+        // every open PR at startup anyway.
         if (lastUpdatedAt == null) {
             var lastUpdatedIssue = issueProject.lastUpdatedIssue();
             if (lastUpdatedIssue.isPresent()) {
