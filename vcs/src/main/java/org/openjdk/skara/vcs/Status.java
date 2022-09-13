4c71d50fec73728136926f7b471fa74604a1b662
Zhao Song <zhao.song@oracle.com>
2022-09-13 16:50:41 -0700

    make parser can handle Status T

--- vcs/src/main/java/org/openjdk/skara/vcs/Status.java
+++ vcs/src/main/java/org/openjdk/skara/vcs/Status.java
@@ -31,7 +31,8 @@ public class Status {
         RENAMED,
         COPIED,
         MODIFIED,
-        UNMERGED
+        UNMERGED,
+        FILE_TYPE_CHANGED
     }
 
     private Operation op;
@@ -66,6 +67,10 @@ public class Status {
         return op == Operation.UNMERGED;
     }
 
+    public boolean isFileTypeChanged() {
+        return op == Operation.FILE_TYPE_CHANGED;
+    }
+
     public int score() {
         return score;
     }
@@ -96,7 +101,7 @@ public class Status {
         }
 
         if (c == 'T') {
-            throw new IllegalArgumentException("It is illegal to change the file type(Reminder: It is also illegal to submit symlink or executable file)");
+            return new Status(Operation.FILE_TYPE_CHANGED, -1);
         }
 
         throw new IllegalArgumentException("Invalid status character: " + c);
@@ -121,7 +126,7 @@ public class Status {
             return new Status(Operation.UNMERGED, -1);
         }
         if (c == 'T') {
-            throw new IllegalArgumentException("It is illegal to change the file type(Reminder: It is also illegal to submit symlink or executable file)");
+            return new Status(Operation.FILE_TYPE_CHANGED, -1);
         }
 
         var score = 0;
@@ -156,6 +161,8 @@ public class Status {
                 return "M";
             case UNMERGED:
                 return "U";
+            case FILE_TYPE_CHANGED:
+                return "T";
             case RENAMED:
                 return "R" + score;
             case COPIED:
