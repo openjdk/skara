9199e7214bfa2cc5519de1b7f8e124493526de8b
Zhao Song <zhao.song@oracle.com>
2023-09-22 11:19:20 -0700

    SKARA-2038

--- bots/pr/src/main/java/org/openjdk/skara/bots/pr/LimitedCensusInstance.java
+++ bots/pr/src/main/java/org/openjdk/skara/bots/pr/LimitedCensusInstance.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2022, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -106,7 +106,7 @@ class LimitedCensusInstance {
                         confOverrideRef);
             }
             return configuration;
-        } catch (UncheckedRestException e) {
+        } catch (UncheckedRestException | UncheckedIOException e) {
             throw e;
         } catch (RuntimeException e) {
             throw new InvalidJCheckConfException(e);
