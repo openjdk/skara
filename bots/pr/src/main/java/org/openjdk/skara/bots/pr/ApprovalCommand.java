ab9a2e70f600ac3ff85ccad33f4de0a1ca7dd106
Zhao Song <zsong@openjdk.org>
2023-09-29 17:21:49 +0000

    2042: Allow bulk requests with /approval request command
    Reviewed-by: erikj

--- bots/pr/src/main/java/org/openjdk/skara/bots/pr/ApprovalCommand.java
+++ bots/pr/src/main/java/org/openjdk/skara/bots/pr/ApprovalCommand.java
@@ -22,18 +22,15 @@
  */
 package org.openjdk.skara.bots.pr;
 
-import org.openjdk.skara.bots.common.SolvesTracker;
 import org.openjdk.skara.forge.PullRequest;
 import org.openjdk.skara.issuetracker.Comment;
-import org.openjdk.skara.vcs.openjdk.Issue;
 
 import java.io.PrintWriter;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.regex.Pattern;
 
 import static org.openjdk.skara.bots.common.CommandNameEnum.approval;
+import static org.openjdk.skara.bots.pr.ApproveCommand.getIssues;
 
 public class ApprovalCommand implements CommandHandler {
     @Override
@@ -75,91 +72,65 @@ public class ApprovalCommand implements CommandHandler {
         String option = argMatcher.group(3);
         String message = argMatcher.group(4);
 
-        var issueOpt = getIssue(issueId, pr, allComments, reply);
-        if (issueOpt.isEmpty()) {
+        var issues = getIssues(issueId, pr, allComments, reply);
+        if (issues.isEmpty()) {
             return;
         }
-        var issue = issueOpt.get();
-
-        if (issue.project().isPresent() && !issue.project().get().equalsIgnoreCase(issueProject.name())) {
-            reply.println("Approval can only be requested for issues in the " + issueProject.name() + " project.");
-            return;
-        }
-
-        var issueTrackerIssueOpt = issueProject.issue(issue.shortId());
-        if (issueTrackerIssueOpt.isEmpty()) {
-            reply.println("Can not find " + issue.id() + " in the " + issueProject.name() + " project.");
-            return;
-        }
-        var issueTrackerIssue = issueTrackerIssueOpt.get();
-        var requestLabel = approval.requestedLabel(targetRef);
-        var approvedLabel = approval.approvedLabel(targetRef);
-        var rejectedLabel = approval.rejectedLabel(targetRef);
-        var prefix = "[" + requestLabel + "]";
-        var comments = issueTrackerIssue.comments();
-        var existingComment = comments.stream()
-                .filter(comment -> comment.author().equals(issueProject.issueTracker().currentUser()))
-                .filter(comment -> comment.body().startsWith(prefix))
-                .findFirst();
-
-        var labels = issueTrackerIssue.labelNames();
-        if (option.equals("cancel")) {
-            if (labels.contains(approvedLabel) || labels.contains(rejectedLabel)) {
-                reply.println("The request has already been handled by a maintainer and can no longer be canceled.");
-            } else {
-                issueTrackerIssue.removeLabel(requestLabel);
-                existingComment.ifPresent(issueTrackerIssue::removeComment);
-                reply.println("The approval request has been cancelled successfully.");
+        reply.println();
+        for (var issue : issues) {
+            reply.print(issue.id() + ": ");
+            if (issue.project().isPresent() && !issue.project().get().equalsIgnoreCase(issueProject.name())) {
+                reply.println("Approval can only be requested for issues in the " + issueProject.name() + " project.");
+                continue;
             }
-        } else if (option.equals("request")) {
-            if (labels.contains(approvedLabel)) {
-                reply.println("Approval has already been requested and approved.");
-            } else if (labels.contains(rejectedLabel)) {
-                reply.println("Approval has already been requested and rejected.");
-            } else {
-                var messageToPost = prefix + " Approval Request from " + command.user().fullName() + "\n" + message.trim();
-                if (existingComment.isPresent()) {
-                    if (!existingComment.get().body().equals(messageToPost)) {
-                        Comment comment = issueTrackerIssue.updateComment(existingComment.get().id(), messageToPost);
-                        reply.println("The approval [request](" + issueTrackerIssue.commentUrl(comment) + ") has been updated successfully.");
-                    } else {
-                        reply.println("The approval [request](" + issueTrackerIssue.commentUrl(existingComment.get()) + ") was already up to date.");
-                    }
+
+            var issueTrackerIssueOpt = issueProject.issue(issue.shortId());
+            if (issueTrackerIssueOpt.isEmpty()) {
+                reply.println("Can not be found in the " + issueProject.name() + " project.");
+                continue;
+            }
+            var issueTrackerIssue = issueTrackerIssueOpt.get();
+            var requestLabel = approval.requestedLabel(targetRef);
+            var approvedLabel = approval.approvedLabel(targetRef);
+            var rejectedLabel = approval.rejectedLabel(targetRef);
+            var prefix = "[" + requestLabel + "]";
+            var comments = issueTrackerIssue.comments();
+            var existingComment = comments.stream()
+                    .filter(comment -> comment.author().equals(issueProject.issueTracker().currentUser()))
+                    .filter(comment -> comment.body().startsWith(prefix))
+                    .findFirst();
+
+            var labels = issueTrackerIssue.labelNames();
+            if (option.equals("cancel")) {
+                if (labels.contains(approvedLabel) || labels.contains(rejectedLabel)) {
+                    reply.println("The request has already been handled by a maintainer and can no longer be canceled.");
                 } else {
-                    Comment comment = issueTrackerIssue.addComment(messageToPost);
-                    reply.println("The approval [request](" + issueTrackerIssue.commentUrl(comment) + ") has been created successfully.");
+                    issueTrackerIssue.removeLabel(requestLabel);
+                    existingComment.ifPresent(issueTrackerIssue::removeComment);
+                    reply.println("The approval request has been cancelled successfully.");
                 }
-                issueTrackerIssue.addLabel(requestLabel);
+            } else if (option.equals("request")) {
+                if (labels.contains(approvedLabel)) {
+                    reply.println("Approval has already been requested and approved.");
+                } else if (labels.contains(rejectedLabel)) {
+                    reply.println("Approval has already been requested and rejected.");
+                } else {
+                    var messageToPost = prefix + " Approval Request from " + command.user().fullName() + "\n" + message.trim();
+                    if (existingComment.isPresent()) {
+                        if (!existingComment.get().body().equals(messageToPost)) {
+                            Comment comment = issueTrackerIssue.updateComment(existingComment.get().id(), messageToPost);
+                            reply.println("The approval [request](" + issueTrackerIssue.commentUrl(comment) + ") has been updated successfully.");
+                        } else {
+                            reply.println("The approval [request](" + issueTrackerIssue.commentUrl(existingComment.get()) + ") was already up to date.");
+                        }
+                    } else {
+                        Comment comment = issueTrackerIssue.addComment(messageToPost);
+                        reply.println("The approval [request](" + issueTrackerIssue.commentUrl(comment) + ") has been created successfully.");
+                    }
+                    issueTrackerIssue.addLabel(requestLabel);
+                }
             }
         }
-    }
-
-    private Optional<Issue> getIssue(String issueId, PullRequest pr, List<Comment> allComments, PrintWriter reply) {
-        var titleIssue = Issue.fromStringRelaxed(pr.title());
-        var issues = new ArrayList<String>();
-        titleIssue.ifPresent(value -> issues.add(value.shortId()));
-        issues.addAll(SolvesTracker.currentSolved(pr.repository().forge().currentUser(), allComments, pr.title())
-                .stream()
-                .map(Issue::shortId)
-                .toList());
-        // If there is only one issue associated with the pr, then the user don't need to specify the issueID in command
-        if (issueId == null) {
-            if (issues.size() == 1) {
-                issueId = issues.get(0);
-            } else if (issues.size() == 0) {
-                reply.println("There is no issue associated with this pull request.");
-                return Optional.empty();
-            } else {
-                reply.println("There are multiple issues associated with this pull request, you need to request approval for each one individually.");
-                return Optional.empty();
-            }
-        }
-        Issue issue = new Issue(issueId, null);
-        if (!issues.contains(issue.shortId())) {
-            reply.println("Approval can only be requested for issues that this pull request solves.");
-            return Optional.empty();
-        }
-        return Optional.of(issue);
     }
 
     @Override
