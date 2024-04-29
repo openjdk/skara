a3945c420f39ab0aa59e8e18c5bb433dfcd2e3b2
Zhao Song <zhao.song@oracle.com>
2024-04-29 15:51:26 -0700

    Update bots/pr/src/main/java/org/openjdk/skara/bots/pr/CheckWorkItem.java
    Co-authored-by: Erik Joelsson <37597443+erikj79@users.noreply.github.com>

--- bots/pr/src/main/java/org/openjdk/skara/bots/pr/CheckWorkItem.java
+++ bots/pr/src/main/java/org/openjdk/skara/bots/pr/CheckWorkItem.java
@@ -433,7 +433,7 @@ class CheckWorkItem extends PullRequestWorkItem {
         if (pr.headHash().hex() == null) {
             String text = "The head hash of this pull request is missing.\n" +
                     "Until this is resolved, this pull request cannot be processed.\n" +
-                    "Please try to use the following command to push an empty commit.\n" +
+                    "This is likely caused by a caching problem in the server and can usually be worked around by pushing another commit to the pull request branch. The commit can be empty. Example:\n" +
                     "```bash\n" +
                     "$ git commit --allow-empty -m \"Empty commit\"\n" +
                     "$ git push\n" +
