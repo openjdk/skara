137ec88566106a2acb7c8527c336171a3e0619d2
Zhao Song <zhao.song@oracle.com>
2024-06-05 16:01:35 -0700

    SKARA-2279

--- test/src/main/java/org/openjdk/skara/test/TestPullRequest.java
+++ test/src/main/java/org/openjdk/skara/test/TestPullRequest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2019, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -87,7 +87,7 @@ public class TestPullRequest extends TestIssue implements PullRequest {
         try {
             var review = new Review(ZonedDateTime.now(), user,
                                     verdict, targetRepository.localRepository().resolve(store().sourceRef()).orElseThrow(),
-                                    store().reviews().size(),
+                                    String.valueOf(store().reviews().size()),
                                     body, targetRef);
 
             store().reviews().add(review);
